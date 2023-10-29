0ea6685130864b4222c07220f8e0bf6dec546800
nicolasjohn <nic.john17@gmail.com>
2023-10-28 22:47:22 -0500

    One data file working in s3

--- gather_data.py
+++ gather_data.py
@@ -6,44 +6,61 @@ import boto3
 s3_client = None
 THRESHOLD = 3
 TIME_FORMAT = "%Y-%m-%d %H:%M"
-STARTED_PATH = "./started.csv"
 
 
 def save_locally(started, completed, path):
     with open(path, mode="w") as file:
         writer = csv.writer(file)
-        for key in completed:
-            writer.writerow([key, *[completed[key][key2].strftime(TIME_FORMAT) for key2 in completed[key]]])
-        for key in started:
-            writer.writerow([key, *[started[key][key2].strftime(TIME_FORMAT) for key2 in started[key]]])
+        for bus_id in completed:
+            writer.writerow([bus_id, completed[bus_id]["left_at"].strftime(TIME_FORMAT),
+                             completed[bus_id]["completed_at"].strftime(TIME_FORMAT)])
+        for bus_id in started:
+            writer.writerow([bus_id, started[bus_id]["left_at"].strftime(TIME_FORMAT)])
 
 
-def save_s3(data_path):
+def save_s3(started, completed, data_path):
     global s3_client
-    s3_client.put_object(Body=open(data_path, 'rb'), Bucket='bus-time-lambda-bucket', Key=data_path.split("/")[-1])
+    data = ""
+
+    for bus_id in completed:
+        data += "%s,%s,%s\n" % (bus_id, completed[bus_id]["left_at"].strftime(TIME_FORMAT),
+                                completed[bus_id]["completed_at"].strftime(TIME_FORMAT))
+    for bus_id in started:
+        data += "%s,%s\n" % (bus_id, started[bus_id]["left_at"].strftime(TIME_FORMAT))
+
+    s3_client.put_object(Body=data, Bucket='bus-time-lambda-bucket', Key=data_path.split("/")[-1])
 
 
 def read_data(path, in_lambda=False):
     global s3_client
     started = {}
     completed = {}
+
     if in_lambda:
         s3_client = boto3.client('s3')
-
-        obj = s3_client.get_object(Bucket='bus-time-lambda-bucket', Key='started.csv')
-        lines = [line.split(',') for line in obj['Body'].read().decode('utf-8').strip().split('\r\n')]
-        for vid, start_time in lines:
-            started[vid] = {"left_at": datetime.datetime.strptime(start_time, TIME_FORMAT)}
+        obj = s3_client.get_object(Bucket='bus-time-lambda-bucket', Key=path.split("/")[-1])
+        lines = [line.split(',') for line in obj['Body'].read().decode('utf-8').split('\n')]
+        started, completed = parse_data(lines)
     else:
         if file_exists(path):
             with open(path, mode="r") as file:
                 reader = csv.reader(file)
-                for vals in reader:
-                    if len(vals) == 2:
-                        started[vals[0]] = {"left_at": datetime.datetime.strptime(vals[1], TIME_FORMAT)}
-                    if len(vals) == 3:
-                        completed[vals[0]] = {"left_at": datetime.datetime.strptime(vals[1], TIME_FORMAT),
-                                              "completed_at": datetime.datetime.strptime(vals[2], TIME_FORMAT)}
+                started, completed = parse_data(reader)
+
+    return started, completed
+
+
+def parse_data(lines):
+    started = {}
+    completed = {}
+
+    for vals in lines:
+        if len(vals) == 2:
+            started[vals[0]] = {"left_at": datetime.datetime.strptime(vals[1], TIME_FORMAT)}
+        if len(vals) == 3:
+            completed[vals[0]] = {"left_at": datetime.datetime.strptime(vals[1], TIME_FORMAT),
+                                  "completed_at": datetime.datetime.strptime(vals[2], TIME_FORMAT)}
+
     return started, completed
 
 
@@ -65,7 +82,6 @@ def track_buses(buses, started, completed, data_path, in_lambda=False, log=False
             if bus.vid in started and bus.minutes < THRESHOLD:
                 completed[bus.vid] = {"left_at": started.pop(bus.vid)["left_at"], "completed_at": now_plus(bus.minutes)}
                 updated_data = True
-                updated_data = True
                 print("\t\t* Trip completed since %s < %s" % (bus.minutes, THRESHOLD)) if log else ""
 
     if log:
@@ -74,9 +90,10 @@ def track_buses(buses, started, completed, data_path, in_lambda=False, log=False
             print("\t- %s" % vid)
 
     if updated_data:
-        save_locally(started, completed, data_path)
         if in_lambda:
-            save_s3(data_path)
+            save_s3(started, completed, data_path)
+        else:
+            save_locally(started, completed, data_path)
 
 
 def gather_data(in_lambda=False):
