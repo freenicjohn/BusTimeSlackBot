b8a90ca59e8ff869119dc3d66dd4f613c310243b
Nic <36267675+freenicjohn@users.noreply.github.com>
2023-11-18 01:27:28 -0600

    Updated to use one data file. Big class refactor. (#9)
    Bug fix

--- gather_data.py
+++ gather_data.py
@@ -1,108 +1,13 @@
 from helpers import *
-import csv
-import datetime
-import boto3
-
-s3_client = None
-THRESHOLD = 3
-TIME_FORMAT = "%Y-%m-%d %H:%M"
-
-
-def save_locally(started, completed, path):
-    with open(path, mode="w") as file:
-        writer = csv.writer(file)
-        for bus_id in completed:
-            writer.writerow([bus_id, completed[bus_id]["start"].strftime(TIME_FORMAT),
-                             completed[bus_id]["end"].strftime(TIME_FORMAT)])
-        for bus_id in started:
-            writer.writerow([bus_id, started[bus_id]["start"].strftime(TIME_FORMAT)])
-
-
-def save_s3(started, completed, data_path):
-    global s3_client
-    data = ""
-
-    for bus_id in completed:
-        data += "%s,%s,%s\n" % (bus_id, completed[bus_id]["start"].strftime(TIME_FORMAT),
-                                completed[bus_id]["end"].strftime(TIME_FORMAT))
-    for bus_id in started:
-        data += "%s,%s\n" % (bus_id, started[bus_id]["start"].strftime(TIME_FORMAT))
-
-    s3_client.put_object(Body=data, Bucket='bus-time-lambda-bucket', Key=data_path.split("/")[-1])
-
-
-def read_data(from_stpid, to_stpid, in_lambda=False):
-    global s3_client
-    started = {}
-    completed = {}
-    path = get_data_path(from_stpid, to_stpid)
-
-    if in_lambda:
-        s3_client = boto3.client('s3')
-        try:
-            obj = s3_client.get_object(Bucket='bus-time-lambda-bucket', Key=path.split("/")[-1])
-            lines = [line.split(',') for line in obj['Body'].read().decode('utf-8').split('\n')]
-            started, completed = parse_data(lines)
-        except Exception as e:
-            print(e)
-    else:
-        if file_exists(path):
-            with open(path, mode="r") as file:
-                reader = csv.reader(file)
-                started, completed = parse_data(reader)
-
-    return started, completed
-
-
-def parse_data(lines):
-    started = {}
-    completed = {}
-
-    for vals in lines:
-        if len(vals) == 2:
-            started[vals[0]] = {"start": datetime.datetime.strptime(vals[1], TIME_FORMAT)}
-        if len(vals) == 3:
-            completed[vals[0]] = {"start": datetime.datetime.strptime(vals[1], TIME_FORMAT),
-                                  "end": datetime.datetime.strptime(vals[2], TIME_FORMAT)}
-
-    return started, completed
-
-
-def track_buses(buses, started, completed, from_stpid, to_stpid, in_lambda=False, log=False):
-    updated_data = False
-
-    for bus in buses:
-        # Check departures
-        if bus.departing:
-            if bus.vid not in started and bus.minutes < THRESHOLD:
-                started[bus.vid] = {"start": (datetime.datetime.now() + datetime.timedelta(minutes=bus.minutes))}
-                updated_data = True
-        # Check arrivals
-        if bus.arriving:
-            if bus.vid in started and bus.minutes < THRESHOLD:
-                completed[bus.vid] = {"start": started.pop(bus.vid)["start"], "end": now_plus(bus.minutes)}
-                updated_data = True
-
-    if log:
-        print("From: %s To: %s" % (from_stpid, to_stpid))
-        print("\n".join(["%s | start: %s" % (vid, started[vid]["start"]) for vid in started]))
-        print("\n".join(["%s | start: %s | end: %s" % (vid, completed[vid]["start"], completed[vid]["end"]) for vid in completed]))
-
-    if updated_data:
-        data_path = get_data_path(from_stpid, to_stpid)
-        if in_lambda:
-            save_s3(started, completed, data_path)
-        else:
-            save_locally(started, completed, data_path)
+from TrackBuses import TrackBuses
 
 
 def gather_data(in_lambda=False):
     set_timezone()
-    response = call_cta_api(stpid_string="%s,%s" % (os.environ["from_stpids"], os.environ["to_stpids"]), log=not in_lambda)
-    for from_stpid, to_stpid in zip(os.environ["from_stpids"].split(","), os.environ["to_stpids"].split(",")):
-        buses = extract_bus_info(response, from_stpid, to_stpid)
-        started, completed = read_data(from_stpid, to_stpid, in_lambda)
-        track_buses(buses, started, completed, from_stpid, to_stpid, in_lambda, log=True)
+    api_response = call_cta_api(stpid_string="%s,%s" % (os.environ["from_stpids"], os.environ["to_stpids"]), log=not in_lambda)
+    bus_data = extract_bus_data(api_response)
+    routes = get_routes()
+    TrackBuses(bus_data, routes, in_lambda, log=True)
 
 
 def lambda_handler(event, context):
--- helpers.py
+++ helpers.py
@@ -1,20 +1,29 @@
-import datetime
+from datetime import datetime, timedelta
 import os
 import requests
 import time
 import json
 
 
-class Bus:
+class BusRecord:
     def __init__(self, data):
         self.due = data["prdctdn"] == "DUE"
         self.dly = data["dly"]
         self.minutes = 0 if self.due or self.dly else int(data["prdctdn"])
-        self.departure_time = datetime.datetime.strptime(data["prdtm"], '%Y%m%d %H:%M').strftime('%I:%M %p')
+        self.departure_time = datetime.strptime(data["prdtm"], '%Y%m%d %H:%M').strftime('%I:%M %p')
+        self.rt = data["rt"]
         self.vid = data["vid"]
         self.stpid = data["stpid"]
         self.departing = self.stpid in os.environ["from_stpids"].split(",")
         self.arriving = self.stpid in os.environ["to_stpids"].split(",")
+
+
+def get_routes():
+    routes = {}
+    for from_id, to_id in zip(os.environ["from_stpids"].split(","), os.environ["to_stpids"].split(",")):
+        routes[from_id] = to_id
+
+    return routes
 
 
 def load_secrets():
@@ -22,27 +31,27 @@ def load_secrets():
     f = open("../BusTimeSlackBot_overlays/secrets.json")
     secrets = json.load(f)
     f.close()
-    secret_names = ["slack_webhook", "from_name", "from_stpids", "rt", "cta_api_key", "to_stpids"]
+    secret_names = ["slack_webhook", "from_name", "from_stpids", "rts", "cta_api_key", "to_stpids", "notify_rt",
+                    "notify_stop_name", "notify_stpid"]
     for name in secret_names:
         os.environ[name] = secrets[name]
 
 
 def call_cta_api(stpid_string="", vid="", log=False):
     cta_url = "http://ctabustracker.com/bustime/api/v2/getpredictions?key=%s&vid=%s&stpid=%s&rt=%s&format=json" % \
-              (os.environ["cta_api_key"], vid, stpid_string, os.environ["rt"])
+              (os.environ["cta_api_key"], vid, stpid_string, os.environ["rts"])
     if log:
-        print("Requesting: %s\n" % cta_url)
+        print("Requesting: %s" % cta_url)
     return requests.get(cta_url).json()
 
 
-def extract_bus_info(data, from_stpid, to_stpid):
-    buses = []
+def extract_bus_data(data):
+    bus_data = []
     if "prd" in data["bustime-response"]:
         data = data["bustime-response"]['prd']
-        for bus_data in data:
-            if bus_data["stpid"] in [from_stpid, to_stpid]:
-                buses.append(Bus(bus_data))
-    return buses
+        for bus in data:
+            bus_data.append(BusRecord(bus))
+    return bus_data
 
 
 def set_timezone():
@@ -51,14 +60,8 @@ def set_timezone():
 
 
 def now_plus(minutes):
-    return datetime.datetime.now() + datetime.timedelta(minutes=minutes)
+    return datetime.now() + timedelta(minutes=minutes)
 
 
 def file_exists(path):
     return os.path.exists(path)
-
-
-def get_data_path(from_stpid, to_stpid):
-    path = "%s_%s_%s.csv" % (from_stpid, to_stpid, datetime.datetime.now().strftime("%Y_%m_%d"))
-
-    return path
